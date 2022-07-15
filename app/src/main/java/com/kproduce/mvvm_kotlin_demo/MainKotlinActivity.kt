9661ea33e502c645b30648c7abc4ecc2968fa534
kuanggang <kuanggang@csdn.net>
2022-07-15 14:47:09 +0800

    first commit

---
+++ app/src/main/java/com/kproduce/mvvm_kotlin_demo/MainKotlinActivity.kt
@@ -0,0 +1,11 @@
+package com.kproduce.mvvm_kotlin_demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainKotlinActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
